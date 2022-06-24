308cef3186b92fa4bce38812529eed897300d41d
Timchenko <tbd2504@gmail.com>
2022-06-24 21:26:13 +0300

    add file

---
+++ task_1.py
@@ -0,0 +1,6 @@
+"""Дано два кортежі, напишіть функцію яка з'єднає їх в один dict:
+  Input:
+    coin = ('Bitcoin', 'Ether', 'Ripple', 'Litecoin')
+    code = ('BTC', 'ETH', 'XRP', 'LTC')
+  Output:
+    {'Bitcoin': 'BTC', 'Ether': 'ETH', 'Ripple': 'XRP', 'Litecoin': 'LTC'}"""
