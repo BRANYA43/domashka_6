b1cab1e0c105a3b57f93967fe9a5276eb6e1bd56
Timchenko <tbd2504@gmail.com>
2022-06-24 22:27:16 +0300

    first commit

--- task_4.py
+++ task_4.py
@@ -6,3 +6,32 @@
   1
   '16:21'
   Повернути час допоможе метод time.strftime з аргументом '%H:%M' """
+
+from datetime import datetime
+from time import sleep
+
+
+def countdown_from_three(func):
+    def wrapper(*args, **kwargs):
+        for i in range(3, 0, -1):
+            sleep(1)
+            print(i)
+        func(*args, **kwargs)
+    return wrapper
+
+
+@countdown_from_three
+def what_time_is_it_now():
+    dt = datetime.now()
+    print(dt.strftime('%H:%M'))
+
+
+def main():
+    what_time_is_it_now()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
