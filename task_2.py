9829fa60e54cbca67e23d170378495b7dcadcaf0
Timchenko <tbd2504@gmail.com>
2022-06-24 22:46:51 +0300

    first commit

--- task_2.py
+++ task_2.py
@@ -1,3 +1,10 @@
 """У додатку є json файл. Написати програму, яка прочитає його та порахує загальну тривалість всіх треків в альбомі.
   Базовий кейс - поверне кількість секунд.
   * Дод. ускладнення повернути у вигляді рядка години:хвилини:секунди, прик. 0:41:39"""
+import json
+import time
+
+with open('acdc.json', 'r') as f:
+    file = json.load(f)
+    sum_duration = sum(int(track['duration']) for track in file['album']['tracks']['track'])
+    print(time.strftime('%H:%M:%S', time.gmtime(sum_duration)))
